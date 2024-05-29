0fc40e5dffd6869d0c66be06088e94690e0acd9f
Bhaskar <bhaskarmandal369@gmail.com>
2024-05-30 02:27:19 +0530

    hamburger added

--- hackathon/pages/home.tsx
+++ hackathon/pages/home.tsx
@@ -29,12 +29,21 @@ export default function Home() {
 
     const handleRegisterClick = () => {
         alert("Register button clicked!");
+        // You can add your registration logic here
+    };
+    const handleHamburgerClick = () => {
+        alert("Menu button clicked!");
+        // You can add your registration logic here
     };
 
     return (
         <div className={styles.container}>
             <div className={styles.ellipse1}></div>
-            <div className={styles.rectangle1}></div>
+            <div className={styles.rectangle1}>
+                <div className={styles.menuButton} onClick={handleHamburgerClick}>
+                    <svg fill="#ffffff" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.00024000000000000003" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"></g><g id="SVGRepo_iconCarrier"> <g data-name="Layer 2"> <g data-name="menu"> <rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"></rect> <rect x="3" y="11" width="18" height="2" rx=".95" ry=".95"></rect> <rect x="3" y="16" width="18" height="2" rx=".95" ry=".95"></rect> <rect x="3" y="6" width="18" height="2" rx=".95" ry=".95"></rect> </g> </g> </g></svg>
+                </div>
+            </div>
             <div className={styles.starttext}>
                 <b>Buckle up!</b> <br />
                 Hackathon will start in
@@ -47,8 +56,8 @@ export default function Home() {
                 <b>Prizes, Jobs & Glory</b> <br />
                 awaits you....
             </div>
-            <div className={styles.register} >
-                <div className={styles.registerText} onClick={handleRegisterClick}>
+            <div className={styles.register} onClick={handleRegisterClick}>
+                <div className={styles.registerText}>
                     Register now!
                 </div>
             </div>

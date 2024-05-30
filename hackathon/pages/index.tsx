fc5deeb586e9259b447135788add81b380aa6adc
Bhaskar <bhaskarmandal369@gmail.com>
2024-05-30 18:39:24 +0530

    homepage css improved

--- hackathon/pages/index.tsx
+++ hackathon/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/home.module.css';
 
 export default function Home() {
     const [countdown, setCountdown] = useState('00:00:00:00');
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         const targetDate = new Date('2024-06-01T00:00:00').getTime();
@@ -31,38 +32,42 @@ export default function Home() {
         alert("Register button clicked!");
         // You can add your registration logic here
     };
+
     const handleHamburgerClick = () => {
-        alert("Menu button clicked!");
-        // You can add your registration logic here
+        setMenuOpen(!menuOpen);
     };
 
     return (
         <div className={styles.container}>
             <div className={styles.ecellLogo}></div>
-            <div className={styles.ellipse1}></div>
-            <div className={styles.rectangle1}>
+
+            <div className={`${styles.circleContainer} ${menuOpen ? styles.circleContainerAFTERCLICK : ''}`}>
+                <div className={`${styles.ellipse1} ${menuOpen ? styles.ellipse1AFTERCLICK : ''}`}></div>
+                <div className={`${styles.register} ${menuOpen ? styles.registerAFTERCLICK : ''}`}>
+                    <div className={styles.registerText} onClick={handleRegisterClick}>
+                        Register now!
+                    </div>
+                </div>
+            </div>
+
+            <div className={`${styles.rectangle1} ${menuOpen ? styles.rectangle1AFTERCLICK : ''}`}>
                 <div className={styles.menuButton} onClick={handleHamburgerClick}>
                     <svg fill="#ffffff" viewBox="0 0 24.00 24.00" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff" stroke-width="0.00024000000000000003" transform="rotate(0)matrix(1, 0, 0, 1, 0, 0)"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.048"></g><g id="SVGRepo_iconCarrier"> <g data-name="Layer 2"> <g data-name="menu"> <rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"></rect> <rect x="3" y="11" width="18" height="2" rx=".95" ry=".95"></rect> <rect x="3" y="16" width="18" height="2" rx=".95" ry=".95"></rect> <rect x="3" y="6" width="18" height="2" rx=".95" ry=".95"></rect> </g> </g> </g></svg>
                 </div>
             </div>
-            <div className={styles.starttext}>
+            <div className={`${styles.starttext} ${menuOpen ? styles.starttextAFTERCLICK : ''}`}>
                 <b>Buckle up!</b> <br />
                 Hackathon will start in
             </div>
-
-            <div className={styles.countdownBOX}>
+            <div className={`${styles.countdownBOX} ${menuOpen ? styles.countdownBOXAFTERCLICK : ''}`}>
                 {countdown}
             </div>
 
-            <div className={styles.description}>
+
+            <div className={`${styles.description} ${menuOpen ? styles.descriptionAFTERCLICK : ''}`}>
                 Hack your <b>future,</b><br />
                 <b>Prizes, Jobs & Glory</b> <br />
                 awaits you....
-            </div>
-            <div className={styles.register}>
-                <div className={styles.registerText} onClick={handleRegisterClick}>
-                    Register now!
-                </div>
             </div>
 
         </div>

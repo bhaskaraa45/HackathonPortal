bc3acc07e76eedafa73bf84471a905e27d7f7e58
Bhaskar <bhaskarmandal369@gmail.com>
2024-06-03 03:08:35 +0530

    small modification

--- hackathon/app/components/customModal.tsx
+++ hackathon/app/components/customModal.tsx
@@ -31,7 +31,7 @@ export default function CustomModal({ isOpen, onClose, description, title, time 
           <div
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
-            <div className="relative w-1/3 my-6 mx-auto max-w-3xl">
+            <div className="relative w-96 my-6 mx-auto max-w-3xl">
               {/*content*/}
               <div className="customModal border-0 rounded-lg shadow-lg relative flex flex-col w-full  outline-none focus:outline-none">
                 {/*header*/}

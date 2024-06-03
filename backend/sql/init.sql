9663acc77706864dda946df2cd52c7d3beddc001
Bhaskar <bhaskarmandal369@gmail.com>
2024-06-04 02:22:19 +0530

    sql update

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
     team_id INTEGER NOT NULL,
     isAdmin BOOLEAN DEFAULT FALSE,
     last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    signup_ip VARCHAR(45)
+    signup_ip VARCHAR(45),
     user_agent VARCHAR(45)
 );
 
@@ -13,32 +13,31 @@ CREATE TABLE teams (
     id SERIAL PRIMARY KEY,
     name VARCHAR NOT NULL UNIQUE,
     current_round INTEGER NOT NULL DEFAULT 1,
-    last_submission INTEGER
-    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_submission INTEGER,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE questions(
+CREATE TABLE questions (
     id SERIAL PRIMARY KEY,
     statement VARCHAR NOT NULL,
     hints jsonb
 );
 
-CREATE TABLE submissions(
+CREATE TABLE submissions (
     team_id INTEGER PRIMARY KEY,
     round_one VARCHAR,
     round_two VARCHAR,
-    round_three VARCHAR
-    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    round_three VARCHAR,
+    last_modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
--- a foreign key constraint to link users.team_id with teams.id
+-- Add foreign key constraint to link users.team_id with teams.id
 ALTER TABLE users
 ADD CONSTRAINT fk_team
 FOREIGN KEY (team_id)
 REFERENCES teams(id);
 
--- a foreign key constraint to link submissions.team_id with teams.id
+-- Add foreign key constraint to link submissions.team_id with teams.id
 ALTER TABLE submissions
 ADD CONSTRAINT fk_submissions_team
 FOREIGN KEY (team_id)

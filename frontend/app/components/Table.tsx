479b7d91de2a9ed55b5ccefd5eb36cdc9016668e
Bhaskar <bhaskarmandal369@gmail.com>
2024-07-05 20:29:48 +0530

    more fixes in leaderboard

--- frontend/app/components/Table.tsx
+++ frontend/app/components/Table.tsx
@@ -13,12 +13,18 @@ type LeaderboardProps = {
 
 const LeaderboardTable: React.FC<LeaderboardProps> = ({ teams }) => {
     return (
-        <Box  width="100%" px="18px" mt="18px" borderRadius="md">
+        <Box width="100%" px="18px" mt="18px" borderRadius="md">
             <VStack spacing="4" width="100%">
                 <HStack height="64px" width="100%" justifyContent="space-between" bg="#5134A4" p="4" px={{ base: 4, md: 8 }} borderRadius="8px">
-                    <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Rank</Text>
-                    <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Team name</Text>
-                    <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Round</Text>
+                    <Box w="54px" >
+                        <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Rank</Text>
+                    </Box>
+                    <Box>
+                        <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Team name</Text>
+                    </Box>
+                    <Box w="68px">
+                        <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Round</Text>
+                    </Box>
                 </HStack>
                 {teams.map((team, index) => (
                     <HStack
@@ -33,9 +39,15 @@ const LeaderboardTable: React.FC<LeaderboardProps> = ({ teams }) => {
                         border="2px solid"
                         borderColor="#1D1E37"
                     >
-                        <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" ml={{ base: "24px", md: "32px" }} color="white">{team.rank}</Text>
-                        <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" color="white">{team.name}</Text>
-                        <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" mr={{ base: "28px", md: "36px" }} color="white">{team.currentRound}</Text>
+                        <Box display="flex" justifyContent="center" alignContent="center" w="54px">
+                            <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" color="white">{team.rank}</Text>
+                        </Box>
+                        <Box>
+                            <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" color="white">{team.name}</Text>
+                        </Box>
+                        <Box display="flex" justifyContent="center" alignContent="center" w="68px">
+                            <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" color="white">{team.currentRound}</Text>
+                        </Box>
                     </HStack>
                 ))}
             </VStack>

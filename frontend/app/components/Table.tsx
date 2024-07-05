c95a1a5eb5f21d53e2c2d1c929c13948d8046be7
Bhaskar <bhaskarmandal369@gmail.com>
2024-07-05 20:22:25 +0530

    fix: leaderboard

--- frontend/app/components/Table.tsx
+++ frontend/app/components/Table.tsx
@@ -13,7 +13,7 @@ type LeaderboardProps = {
 
 const LeaderboardTable: React.FC<LeaderboardProps> = ({ teams }) => {
     return (
-        <Box width="100%" mx="auto" p="8" borderRadius="md">
+        <Box  width="100%" px="18px" mt="18px" borderRadius="md">
             <VStack spacing="4" width="100%">
                 <HStack height="64px" width="100%" justifyContent="space-between" bg="#5134A4" p="4" px={{ base: 4, md: 8 }} borderRadius="8px">
                     <Text fontSize="1.25rem" fontWeight="medium" color="white" noOfLines={1}>Rank</Text>
@@ -33,9 +33,9 @@ const LeaderboardTable: React.FC<LeaderboardProps> = ({ teams }) => {
                         border="2px solid"
                         borderColor="#1D1E37"
                     >
-                        <Text fontSize="1.25rem" fontWeight="medium" ml={{base: "24px", md:"32px"}} color="white">{team.rank}</Text>
-                        <Text fontSize="1.25rem" fontWeight="medium" color="white">{team.name}</Text>
-                        <Text fontSize="1.25rem" fontWeight="medium" mr={{base: "28px", md:"36px"}} color="white">{team.currentRound}</Text>
+                        <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" ml={{ base: "24px", md: "32px" }} color="white">{team.rank}</Text>
+                        <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" color="white">{team.name}</Text>
+                        <Text noOfLines={1} fontSize="1.25rem" fontWeight="medium" mr={{ base: "28px", md: "36px" }} color="white">{team.currentRound}</Text>
                     </HStack>
                 ))}
             </VStack>
